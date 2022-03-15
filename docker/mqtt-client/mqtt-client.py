843695794f571f9440eb0d6f31da9a70aac697d0
Lukas Hoffmann <lukas.hoffmann.1@campus.tu-berlin.de>
2022-03-15 22:55:36 +0100

    fix issues with mqtt client und sub

--- docker/mqtt-client/mqtt-client.py
+++ docker/mqtt-client/mqtt-client.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
 
         client.connect(BROKER_ADDRESS, PORT)
 
-        print("Connected to MQTT Broker: " + BROKER_ADDRESS, file = sys.stderr)
+        print("Connected to MQTT Broker: " + BROKER_ADDRESS, file = sys.stdout)
 
         DATA = dt_string = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
